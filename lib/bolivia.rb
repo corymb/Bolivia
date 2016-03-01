30b896991bc0f80803722a0cbbeb31a4c0c9eb45
Dave Bush <github@david-bush.co.uk>
2016-03-01 08:02:04 +0000

    Added raise404 method.

--- lib/bolivia.rb
+++ lib/bolivia.rb
@@ -4,4 +4,14 @@ module Bolivia
   def generate_secret_key
     SecureRandom.hex(50)
   end
+
+  def raise404
+      res.status = 404
+      if File.exist?('views/404.mote')
+        res.write view("404")
+      else
+        res.write 'Page not found.'
+        halt(res.finish)
+      end
+  end
 end
