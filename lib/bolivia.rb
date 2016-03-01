4adb70be68dc906fe514476c7d2d93f7a4d65d0f
Dave Bush <github@david-bush.co.uk>
2016-03-01 22:55:37 +0000

    Fixed indentation, added comments and added to_json method.

--- lib/bolivia.rb
+++ lib/bolivia.rb
@@ -2,16 +2,25 @@ require "bolivia/version"
 
 module Bolivia
   def generate_secret_key
+    # Generates a 50 character alphanumeric secret key:
     SecureRandom.hex(50)
   end
 
-  def raise404
-      res.status = 404
-      if File.exist?('views/404.mote')
-        res.write view("404")
-      else
-        res.write 'Page not found.'
-        halt(res.finish)
-      end
+  def raise404(message='Page not found.')
+    # Renders views/404.mote if it exists;
+    # otherwise returns 'page not found'
+    res.status = 404
+    if File.exist?('views/404.mote')
+      res.write view("404")
+    else
+      res.write message
+      halt(res.finish)
+    end
+  end
+
+  def to_json(data)
+    # Returns data as a JSON response:
+    res.headers["Content-Type"] = "application/json; charset=utf-8"
+    res.write data.to_json
   end
 end
