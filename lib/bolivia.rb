43ca2dc8c8eee65631e28d7708f51cdde03a9f77
Dave Bush <github@david-bush.co.uk>
2016-03-01 07:05:22 +0000

    Created Bolivia gem.

---
+++ lib/bolivia.rb
@@ -0,0 +1,5 @@
+require "bolivia/version"
+
+module Bolivia
+  # Your code goes here...
+end
---
+++ spec/bolivia_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Bolivia do
+  it 'has a version number' do
+    expect(Bolivia::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
